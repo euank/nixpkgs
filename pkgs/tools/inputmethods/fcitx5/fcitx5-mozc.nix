8cd0a633891765e293e8685886c5cbcba219278c
Euan Kemp <euank@euank.com>
2022-02-27 23:53:14 -0800

    fcitx5-mozc: 2.26.4220.102 -> 2.26.4660.102

--- pkgs/tools/inputmethods/fcitx5/fcitx5-mozc.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-mozc.nix
@@ -1,14 +1,8 @@
 { lib, clangStdenv, fetchFromGitHub, fetchurl, fetchpatch, fetchgit
 , python3Packages, ninja, pkg-config, protobuf, zinnia, qt5, fcitx5
-, jsoncpp, gtest, which, gtk2, unzip, abseil-cpp, breakpad }:
+, jsoncpp, which, gtk2, unzip, abseil-cpp, breakpad }:
 let
   inherit (python3Packages) python gyp six;
-  japanese_usage_dictionary = fetchFromGitHub {
-    owner = "hiroyuki-komatsu";
-    repo = "japanese-usage-dictionary";
-    rev = "e5b3425575734c323e1d947009dd74709437b684";
-    sha256 = "0pyrpz9c8nxccwpgyr36w314mi8h132cis8ijvlqmmhqxwsi30hm";
-  };
   zipcode_rel = "202011";
   jigyosyo = fetchurl {
     url = "https://osdn.net/projects/ponsfoot-aur/storage/mozc/jigyosyo-${zipcode_rel}.zip";
@@ -22,40 +16,23 @@ let
 
 in clangStdenv.mkDerivation rec {
   pname = "fcitx5-mozc";
-  version = "2.26.4220.102";
+  version = "2.26.4660.102";
 
   src = fetchFromGitHub {
     owner = "fcitx";
     repo = "mozc";
-    rev = "1882e33b61673b66d63277f82b4c80ae4e506c10";
-    sha256 = "R+w0slVFpqtt7PIr1pyupJjRoQsABVZiMdZ9fKGKAqw=";
+    rev = "cf8d220b7d9b4fb3ca4345bf459f6680756e1658";
+    sha256 = "sha256-17mGQ7wioLsMKXBHWwhF04wBJSnPbu/KjNBUKXKfYLM=";
+    fetchSubmodules = true;
   };
 
-  nativeBuildInputs = [ gyp ninja python pkg-config qt5.wrapQtAppsHook six which unzip ];
+  nativeBuildInputs = [ ninja python pkg-config qt5.wrapQtAppsHook six which unzip ];
 
-  buildInputs = [ protobuf zinnia qt5.qtbase fcitx5 abseil-cpp jsoncpp gtest gtk2 ];
-
-  patches = [
-    # Support linking system abseil-cpp
-    (fetchpatch {
-      url = "https://salsa.debian.org/debian/mozc/-/raw/debian/sid/debian/patches/0007-Update-src-base-absl.gyp.patch";
-      sha256 = "UiS0UScDKyAusXOhc7Bg8dF8ARQQiVTylEhAOxqaZt8=";
-    })
-
-  ];
+  buildInputs = [ protobuf zinnia qt5.qtbase fcitx5 gtk2 ];
 
   postUnpack = ''
     unzip ${x-ken-all} -d $sourceRoot/src/
     unzip ${jigyosyo} -d $sourceRoot/src/
-
-    rmdir $sourceRoot/src/third_party/breakpad/
-    ln -s ${breakpad} $sourceRoot/src/third_party/breakpad
-    rmdir $sourceRoot/src/third_party/gtest/
-    ln -s ${gtest} $sourceRoot/src/third_party/gtest
-    rmdir $sourceRoot/src/third_party/gyp/
-    ln -s ${gyp} $sourceRoot/src/third_party/gyp
-    rmdir $sourceRoot/src/third_party/japanese_usage_dictionary/
-    ln -s ${japanese_usage_dictionary} $sourceRoot/src/third_party/japanese_usage_dictionary
   '';
 
   # Copied from https://github.com/archlinux/svntogit-community/blob/packages/fcitx5-mozc/trunk/PKGBUILD
