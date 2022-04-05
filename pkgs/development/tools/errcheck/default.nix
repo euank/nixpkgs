d9c62645dc36c333eef459e6c7370d2274b5f818
Euan Kemp <euan@ngrok.com>
2022-04-05 20:35:43 +0000

    pkgs/errcheck: bump for go1.18 support

--- pkgs/development/tools/errcheck/default.nix
+++ pkgs/development/tools/errcheck/default.nix
@@ -2,16 +2,17 @@
 
 buildGoModule rec {
   pname = "errcheck";
-  version = "1.6.0";
+  rev = "e62617a91f7bd1abab2cbe7f28966188dd85eee0";
+  version = "20220325-${lib.strings.substring 0 7 rev}";
 
   src = fetchFromGitHub {
     owner = "kisielk";
     repo = "errcheck";
-    rev = "v${version}";
-    sha256 = "sha256-Przf2c2jFNdkUq7IOUD7ChXHiSayAz4xTsNzajycYZ0=";
+    inherit rev;
+    sha256 = "sha256-RoPv6Odh8l9DF1S50pNEomLtI4uTDNjveOXZd4S52c0=";
   };
 
-  vendorSha256 = "sha256-rluaBdW+w2zPThELlBwX/6LXDgc2aIk/ucbrsrABpVc=";
+  vendorSha256 = "sha256-fDugaI9Fh0L27yKSFNXyjYLMMDe6CRgE6kVLiJ3+Kyw=";
 
   meta = with lib; {
     description = "Program for checking for unchecked errors in go programs";
