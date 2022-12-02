c8d79a87163533d750573ce7d897cae9cf126a18
Euan Kemp <euank@euank.com>
2022-12-01 23:23:38 -0800

    more updates

--- pkgs/applications/editors/vim/plugins/default.nix
+++ pkgs/applications/editors/vim/plugins/default.nix
@@ -40,7 +40,7 @@ let
   extensible-self = lib.makeExtensible
     (extends aliases
       (extends overrides
-        (extends (plugins // denops) initialPackages)
+        (extends (extends (plugins) initialPackages) denops)
       )
     );
 in
