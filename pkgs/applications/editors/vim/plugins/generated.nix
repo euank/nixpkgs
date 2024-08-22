77474b38c9d81e05d1a64261a57403b4e7f3ad63
Euan Kemp <euank@euank.com>
2024-08-23 01:01:20 +0900

    vimPlugins.pum-vim: update to 2024-08-17

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -9354,12 +9354,12 @@ final: prev:
 
   pum-vim = buildVimPlugin {
     pname = "pum.vim";
-    version = "2024-01-18";
+    version = "2024-08-17";
     src = fetchFromGitHub {
       owner = "Shougo";
       repo = "pum.vim";
-      rev = "19988d249e618808404c8d3fe70e7dcc9b51b784";
-      sha256 = "1q0dh929nz5qmfnv2wvz6fmr0rjy72cky5vv6s8c22p4bc249xx4";
+      rev = "d9995c8f134ee84322d3dc6f12ea7f061dcacf3f";
+      sha256 = "19akkaqja5khb3nqcm7yj84735vzkwqlr37k18bj34ncm60z9knw";
     };
     meta.homepage = "https://github.com/Shougo/pum.vim/";
   };
