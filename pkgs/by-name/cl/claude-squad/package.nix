a8423a0a303a81b06a5c669e02b854543fd0bdd5
Euan Kemp <euank@euank.com>
2025-07-21 12:41:04 +0900

    wip

--- pkgs/by-name/cl/claude-squad/package.nix
+++ pkgs/by-name/cl/claude-squad/package.nix
@@ -2,6 +2,8 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  git,
+  writableTmpDirAsHomeHook,
 }:
 
 buildGoModule rec {
@@ -12,16 +14,22 @@ buildGoModule rec {
     owner = "smtg-ai";
     repo = "claude-squad";
     rev = "v${version}";
-    sha256 = "";
+    sha256 = "sha256-oXjVMcobJ4sLh7m9Zc2EAKAL90FZ/3NkA5byfDXJnSk=";
   };
 
-  vendorHash = "sha256-LKJXoXZS866UfJ+Edwf6AkAZmTV2Q1OI1mZfbsxHb3s=";
+  vendorHash = "sha256-BduH6Vu+p5iFe1N5svZRsb9QuFlhf7usBjMsOtRn2nQ=";
+
+  nativeBuildInputs = [
+    # for tests
+    git
+    writableTmpDirAsHomeHook
+  ];
 
   meta = with lib; {
     description = "Manage multiple AI terminal agents like Claude Code, Aider, Codex, OpenCode, and Amp.";
     homepage = "https://smtg-ai.github.io/claude-squad/";
-    license = licenses.agpl3;
+    license = licenses.agpl3Only;
     maintainers = with maintainers; [ euank ];
-    mainProgram = "cs";
+    mainProgram = "claude-squad";
   };
 }
